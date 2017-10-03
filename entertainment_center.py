05da6837aa3909c0a04f39300baf96b8b44d2c3e
Andy Fever <andy.fever@gmail.com>
2017-10-03 20:15:14 +0100

    Working basic website

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,9 @@
 import media
+import fresh_tomatoes
+
 alien = media.Movie("Alien",
                     "A pissed off Alien against the crew of the Nostramo",
-                    'https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiJgpL3htXWAhVCPVAKHUAbBFQQjRwIBw&url=https%3A%2F%2Fwww.sideshowtoy.com%2Fcollectibles%2Faliens-alien-warrior-hot-toys-902693%2F&psig=AOvVaw1nLBUD_aN3MxX_GKUd-GIP&ust=1507140737477260',
+                    'http://images.fromupnorth.com/216/54a2e3cb62f02.jpg',
                     'https://www.youtube.com/watch?v=bEVY_lonKf4')
 
 rear_window = media.Movie("Rear Window",
@@ -9,4 +11,26 @@ rear_window = media.Movie("Rear Window",
                           "http://img.moviepostershop.com/rear-window-movie-poster-1954-1020143868.jpg",
                           "https://www.youtube.com/watch?v=Ob_Sq__g01E")
 
-rear_window.show_trailer()
+school_of_rock = media.Movie("School of Rock",
+                             "A sub corrupts kids into Rock n Roll",
+                             "https://fanart.tv/fanart/movies/1584/movieposter/school-of-rock-53f7a862c918b.jpg",
+                             "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+
+inception = media.Movie("Inception",
+                        "A dream within a dream within a dream (repeat as required)",
+                        "http://www.impawards.com/2010/posters/inception.jpg",
+                        "https://www.youtube.com/watch?v=8hP9D6kZseM")
+
+wonder_woman = media.Movie("Wonder Woman",
+                        "Who new Ares was responsible for WW1?",
+                        "http://www.joblo.com/posters/images/full/dianabrace.jpg",
+                        "https://www.youtube.com/watch?v=VSB4wGIdDwo")
+
+star_wars = media.Movie("Star Wars",
+                        "A kid with daddy issues battles the empire",
+                        "https://images-na.ssl-images-amazon.com/images/I/81P3lDJbjCL._SL1347_.jpg",
+                        "https://www.youtube.com/watch?v=ce6BFgwazMg")
+
+
+movies = [alien, rear_window, school_of_rock, inception, wonder_woman, star_wars]
+fresh_tomatoes.open_movies_page(movies)
