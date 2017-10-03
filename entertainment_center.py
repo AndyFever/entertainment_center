96308ef3350a30ad28f1799b4341817a4156dc9b
Andy Fever <andy.fever@gmail.com>
2017-10-03 19:53:48 +0100

    Initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,12 @@
+import media
+alien = media.Movie("Alien",
+                    "A pissed off Alien against the crew of the Nostramo",
+                    'https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiJgpL3htXWAhVCPVAKHUAbBFQQjRwIBw&url=https%3A%2F%2Fwww.sideshowtoy.com%2Fcollectibles%2Faliens-alien-warrior-hot-toys-902693%2F&psig=AOvVaw1nLBUD_aN3MxX_GKUd-GIP&ust=1507140737477260',
+                    'https://www.youtube.com/watch?v=bEVY_lonKf4')
+
+rear_window = media.Movie("Rear Window",
+                          "Peeping Tom solves a murder",
+                          "http://img.moviepostershop.com/rear-window-movie-poster-1954-1020143868.jpg",
+                          "https://www.youtube.com/watch?v=Ob_Sq__g01E")
+
+rear_window.show_trailer()
